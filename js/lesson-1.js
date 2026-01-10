2bd9d12657769f3dbb4d141e4bc75d40199a6ecb
vitaliifedunyk <vitalik.fedunyk@gmail.com>
2026-01-10 12:04:18 +0100

    Add commented-out example for formatting time from total minutes

--- js/lesson-1.js
+++ js/lesson-1.js
@@ -74,3 +74,15 @@
 // Приклад: користувач вводить в prompt '70' -> в консолі відобразиться '01:10'.
 // Корисне посилання для відображення годин та хвилин у потрібному форматі ('01' замість '1'):
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/padStart#examples
+
+// const totalMinutes = Number(prompt("Введіть кількість хвилин"));
+
+// function formatTime(totalMinutes) {
+//   const hours = Math.floor(totalMinutes / 60);
+//   const modifiedHours = String(hours).padStart(2, "0");
+//   const minutes = totalMinutes % 60;
+//   const modifiedMinutes = String(minutes).padStart(2, "0");
+//   return `${modifiedHours}:${modifiedMinutes}`;
+// }
+
+// console.log(formatTime(totalMinutes));
