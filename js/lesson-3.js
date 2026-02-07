330d5b7ac9995a2131f7989a3c0cc001820fffcc
vitaliifedunyk <vitalik.fedunyk@gmail.com>
2026-02-07 14:03:42 +0100

    task-8

--- js/lesson-3.js
+++ js/lesson-3.js
@@ -86,11 +86,15 @@
 //  Відсортуйте масив об'єктів за віком у порядку зростання. 
 // Очікуваний результат: [{name: "Bob", age: 19}, {name: "John", age: 27}, {name: "Jane", age: 31}].
 
-//const users = [
-//  { name: 'John', age: 27 },
-//  { name: 'Jane', age: 31 },
-//  { name: 'Bob', age: 19 },
+// const users = [
+//   { name: 'John', age: 27 },
+//   { name: 'Jane', age: 31 },
+//   { name: 'Bob', age: 19 },
 // ];
+
+// const res = users.toSorted((a, b) => a.age - b.age)
+// console.log(res);
+
 
 //!========================================================================
 
