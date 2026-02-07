28e6514873851d2deeb4a508c3fbdedf6bbde798
vitaliifedunyk <vitalik.fedunyk@gmail.com>
2026-02-07 11:17:20 +0100

    task-11

--- js/practice-module-3.js
+++ js/practice-module-3.js
@@ -441,11 +441,11 @@
 
 // Завдання 28: Array from String
 // Зробіть масив літер з рядка, але тільки голосних (a, e, i, o, u).
-const t28_str = "hello world";
-let vowel = "aeiou"
+// const t28_str = "hello world";
+// let vowel = "aeiou"
 
-const res = [...t28_str].filter(leter => vowel.includes(leter))
-console.log(res);
+// const res = [...t28_str].filter(letter => vowel.includes(letter))
+// console.log(res);
 
 // Очікується: ['e', 'o', 'o']
 
@@ -618,23 +618,3 @@ console.log(res);
 // // ]
 // // Підказка: Спочатку зробіть частотний словник (як в завданні 20),
 // // а потім map або Object.entries.
-
-
-// function greet(name) {
-//     console.log(`Welcome ${name}!`);
-// }
-
-// function notify(name) {
-//     console.log(`Dear ${name}, your room will be ready in 30 minutes`);
-// }
-
-// function registerGuest(name, callback) {
-//     console.log(`Registering ${name}!`);
-//     callback(name);
-// }
-
-// registerGuest("Mango", greet); // "Registering Mango!"
-// // "Welcome Mango!"
-
-// registerGuest("Mango", notify); // "Registering Mango!"
-// // "Dear Mango, your room will be ready in 30 minutes"
