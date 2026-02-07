4b54bcd2070fdbaf0b0d3b4109f0466804997653
vitaliifedunyk <vitalik.fedunyk@gmail.com>
2026-02-07 11:21:03 +0100

    task-1

--- js/lesson-3.js
+++ js/lesson-3.js
@@ -6,6 +6,8 @@
 
 // const numbers = [1, 2, 3, 4, 5]
 
+// const result = numbers.map(num => num * num)
+// console.log(result);
 
 //!========================================================================
 
